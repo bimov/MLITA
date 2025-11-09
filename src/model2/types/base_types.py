3c9a9e3f177943349924da5547141816d9d04d76
Alexundr38 <4sunwarrior1@mail.ru>
2025-11-09 15:48:02 +0300

    add_parser

--- src/model2/types/base_types.py
+++ src/model2/types/base_types.py
@@ -1,25 +1,48 @@
 from enum import Enum
 from typing import Union
 
+
 class LogicalConnectives(Enum):
-    CONJUNCTION = '∧'
-    DISJUNCTION = '∨'
-    IMPLICATION = '→'
+    AND = '∧'
+    OR = '∨'
+    XOR = "⊕"
+    THEN = "→"
+    IFF = "↔️"
+
+    @classmethod
+    def get(self, value: str):
+        if value == '∧': return LogicalConnectives.AND
+        if value == '∨': return LogicalConnectives.OR
+        if value == "⊕": return LogicalConnectives.XOR
+        if value == "→": return LogicalConnectives.THEN
+        if value == "↔️": return LogicalConnectives.IFF
+
 
 class Quantifiers(Enum):
     UNIVERSALITY = '∀'
     EXISTENCE = '∃'
 
+    @classmethod
+    def get(self, value: str):
+        if value == '∀': return Quantifiers.UNIVERSALITY
+        if value == '∃': return Quantifiers.EXISTENCE
+
+
 variables = []
 
 
 class Atom:
-    def __init__(self, func: str, values: Union[list[str], str]):
+    def __init__(self, func: str, values: Union[list[str], str, list['Atom'], 'Atom', list[Union[str, 'Atom']]]):
         self.func = func
-        if type(values) is type(str) :
+        if not isinstance(values, list):
             self.values = [values]
         else:
             self.values = values
+
+    def __repr__(self):
+        if len(self.values) > 0:
+            return f"{self.func}({', '.join(map(str, self.values))})"
+        return f"{self.func}{', '.join(map(str, self.values))}"
 
 
 class Formula:
@@ -30,12 +53,18 @@ class AtomicFormula(Formula):
     def __init__(self, atom: Atom):
         self.atom = atom
 
+    def __repr__(self):
+        return str(self.atom)
+
 
 class QuantifiedFormula(Formula):
-    def __init__(self, quantifiers: Quantifiers, variable: str, formula: AtomicFormula):
-        self.quantifiers = quantifiers
+    def __init__(self, quantifier: Quantifiers, variable: str, formula: Formula):
+        self.quantifier = quantifier
         self.variable = variable
         self.formula = formula
+
+    def __repr__(self):
+        return f"{self.quantifier.value}{self.variable}({self.formula})"
 
 
 class BinaryFormula(Formula):
@@ -44,8 +73,13 @@ class BinaryFormula(Formula):
         self.connective = connective
         self.right = right
 
+    def __repr__(self):
+        return f"({self.left} {self.connective.value} {self.right})"
+
 
 class NegativeFormula(Formula):
     def __init__(self, formula: Formula):
         self.formula = formula
 
+    def __repr__(self):
+        return f"¬{self.formula}"
